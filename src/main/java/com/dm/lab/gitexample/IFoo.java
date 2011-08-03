e5168c271bc9ad3a361c6f513df0934118d1c143
Daniel <dmroczka@gmail.com>
2011-08-03 02:00:14 +0200

    Added file

---
+++ src/main/java/com/dm/lab/gitexample/IFoo.java
@@ -0,0 +1,11 @@
+package com.dm.lab.gitexample;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: daniel
+ * Date: 03.08.11
+ * Time: 01:59
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IFoo {
+}
